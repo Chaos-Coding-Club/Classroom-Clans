bbd1578367ff1c91db867aae227b777b26b1001d
Ishan Gohil <86436938+IshanGProjects@users.noreply.github.com>
2024-03-03 07:34:36 -0700

    added chnages for drawer color

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,23 +1,29 @@
 import { Redirect } from "expo-router";
 import { Drawer } from "expo-router/drawer";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-
+import { StyleSheet, useColorScheme } from "react-native";
 import { useAuth } from "@/contexts/AuthContext";
 
 export default function AppLayout() {
   const auth = useAuth();
+  const colorScheme = useColorScheme();
 
   // Only require authentication within the (app) group's layout as users
   // need to be able to access the (auth) group and sign in again.
+
   if (!auth.currentUser) {
     // On web, static rendering will stop here as the user is not authenticated
     // in the headless Node process that the pages are rendered in.
     return <Redirect href="/Login/" />;
   }
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer initialRouteName="Home/index">
+      <Drawer
+        screenOptions={{
+          headerTintColor: colorScheme === "light" ? "black" : "white",
+        }}
+        initialRouteName="Home/index"
+      >
         <Drawer.Screen
           name="Home/index" // This is the name of the page and must match the url from root
           options={{
